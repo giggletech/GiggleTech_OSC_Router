3bad625ca37de2bd6fdb411aad2a4c2986d26e5a
lizard-brain <37095041+lizard-brain@users.noreply.github.com>
2023-05-03 07:25:16 +1000

    Working Clean

--- giggletech-router/src/main.rs
+++ giggletech-router/src/main.rs
@@ -19,130 +19,6 @@ mod osc_timeout;
 
 
 
-/* 
-
-#[async_std::main]
-async fn main() -> Result<()> {
-     
-    // Import Config 
-    // Todo: Refactor
-    let (headpat_device_ip,
-        headpat_device_uris,
-        min_speed,
-        mut max_speed,
-        speed_scale,
-        port_rx,
-        proximity_parameter_address,
-        proximity_parameters_multi,
-        max_speed_parameter_address,
-        max_speed_low_limit,
-
-    ) = config::load_config();
-
-    let headpat_device_ip_arc_2_orig = Arc::new("192.168.1.153".to_string());
-    let headpat_device_ip_arc_2 = &Arc::clone(&headpat_device_ip_arc_2_orig);
-
-    let proximity_parameter_address_2 = "/avatar/parameters/proximity_02";
-    // Rx/Tx Socket Setup
-    let mut rx_socket = giggletech_osc::setup_rx_socket(port_rx).await?;
-
- 
-    // Timeout
-    let headpat_device_ip_clone = headpat_device_ip.clone(); 
-    task::spawn(async move {
-        osc_timeout(&headpat_device_ip_clone).await.unwrap();
-    });
-
-
-
-    // Start/ Stop Function Setup
-    let running = Arc::new(AtomicBool::new(false));
-    let headpat_device_ip_arc = Arc::new(headpat_device_ip);
-
-    // create iterators that iterartos over list
-
-    // Listen for OSC Packets
-    while let Some(packet) = rx_socket.next().await {
-        let (packet, _peer_addr) = packet?;
-        
-        // Filter OSC Signals : Headpat Max & Headpat Prox 
-        match packet {
-            OscPacket::Bundle(_) => {}
-            OscPacket::Message(message) => {
-
-                let (address, osc_value) = message.as_tuple();
-                let value = match osc_value.first().unwrap_or(&OscType::Nil).clone().float(){
-                    Some(v) => v, 
-                    None => continue,
-                };
-
-                // Max Speed Setting
-                if address == max_speed_parameter_address {
-                    data_processing::print_speed_limit(value);
-                    max_speed = value.max(max_speed_low_limit);
-                }
-                
-                // Prox Parmeter 1
-                else if address == proximity_parameter_address  {
-                    
-                    terminator::stop(running.clone()).await?;
-                    // Update Last Signal Time for timeout clock
-                    let mut last_signal_time = osc_timeout::LAST_SIGNAL_TIME.lock().unwrap();
-                    *last_signal_time = Instant::now();
-
-                    // Stop Function
-                    if value == 0.0 {
-                        println!("Stopping pats...");
-                        terminator::start(running.clone(), &headpat_device_ip_arc).await?;
-
-                        for _ in 0..5 {
-                            giggletech_osc::send_data(&headpat_device_ip_arc, 0i32).await?;  
-                        }
-
-                    } else {
-                        giggletech_osc::send_data(&headpat_device_ip_arc,
-                            data_processing::process_pat(value, max_speed, min_speed, speed_scale)).await?;
-                    }
-                }
-
-               // Prox Parmeter 2
-               else if address == proximity_parameter_address_2  {
-               //else if address == proximity_parameter_address  {
-                    
-                terminator::stop(running.clone()).await?;
-                // Update Last Signal Time for timeout clock
-                let mut last_signal_time = osc_timeout::LAST_SIGNAL_TIME.lock().unwrap();
-                *last_signal_time = Instant::now();
-
-                // Stop Function
-                if value == 0.0 {
-                    println!("Stopping pats...");
-                    terminator::start(running.clone(), &headpat_device_ip_arc_2).await?;
-
-                    for _ in 0..5 {
-                        giggletech_osc::send_data(&headpat_device_ip_arc_2, 0i32).await?;  
-                    }
-
-                } else {
-                    giggletech_osc::send_data(&headpat_device_ip_arc_2,
-                        data_processing::process_pat(value, max_speed, min_speed, speed_scale)).await?;
-                }
-            }
-
-                else {
-                    //eprintln!("Unknown Address") // Have a debug mode, print if debug mode
-                }
-            } 
-        }  
-    }
-    Ok(())
-}
-
-*/
-
-/* 
-
-*/
 
 
 async fn handle_proximity_parameter(
@@ -168,11 +44,13 @@ async fn handle_proximity_parameter(
             giggletech_osc::send_data(&device_ip, 0i32).await?;  
         }
     } else {
+        //start_timer(value);
         giggletech_osc::send_data(&device_ip,
             data_processing::process_pat(value, max_speed, min_speed, speed_scale)).await?;
     }
     Ok(())
 }
+
 
 
 
