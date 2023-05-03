e71b85b91417e99193fc5b4e13b56429c8969fff
lizard-brain <37095041+lizard-brain@users.noreply.github.com>
2023-05-04 08:02:14 +1000

    user to cycle through device

--- giggletech-router/src/main.rs
+++ giggletech-router/src/main.rs
@@ -44,23 +44,45 @@ async fn main() -> Result<()> {
         timeout,
     ) = config::load_config();
 
+    use std::io::{stdin, stdout, Write};
     use std::thread;
     use std::time::Duration;
     
-    let steps = 10; // set the number of steps here
+    let steps = 20; // set the number of steps here
     
-    for osc_address in &proximity_parameters_multi {
-        for i in 0..2 {
-            for j in 0..=steps {
-                let value = if i == 0 {
-                    j as f32 / steps as f32
-                } else {
-                    (steps - j) as f32 / steps as f32
-                };
-                println!(" {}: value: {}", osc_address, value);
-                giggletech_osc::send_data("127.0.0.1", osc_address, &port_rx, value).await?;
-                thread::sleep(Duration::from_millis(100));
+    loop {
+        let mut input = String::new();
+    
+        for osc_address in &proximity_parameters_multi {
+            for i in 0..2 {
+                for j in 0..=steps {
+                    let value = if i == 0 {
+                        j as f32 / steps as f32
+                    } else {
+                        (steps - j) as f32 / steps as f32
+                    };
+                    println!("sending value: {} to osc_address: {}", value, osc_address);
+                    giggletech_osc::send_data("127.0.0.1", osc_address, &port_rx, value).await?;
+                    thread::sleep(Duration::from_millis(100));
+                }
             }
+            println!("Press enter to test next device");
+            stdout().flush()?;
+            stdin().read_line(&mut input)?;
+            if input.trim() == "" {
+                input.clear();
+            } else {
+                break;
+            }
+        }
+    
+        println!("All devices have been tested. Press enter to test again or type 'quit' to exit");
+        stdout().flush()?;
+        input.clear();
+        stdin().read_line(&mut input)?;
+    
+        if input.trim() == "quit" {
+            break;
         }
     }
     
