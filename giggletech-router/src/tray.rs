8c803602663de5ac40fd7f44aec92955fa41fcbf
Diogo Mateus <diogo.tpr@gmail.com>
2024-03-04 15:38:12 +0000

    Add the open terminal functionality to the tray

--- giggletech-router/src/tray.rs
+++ giggletech-router/src/tray.rs
@@ -1,20 +1,25 @@
-use std::env;
-use log::error;
+use std::{env, process::Command};
 use tray_icon::{
     menu::{accelerator::Accelerator, Menu, MenuEvent, MenuItem},
     TrayIconBuilder,
 };
-use winit::event_loop::{ControlFlow, EventLoopBuilder};
+use winit::{
+    event::Event,
+    event_loop::{ControlFlow, EventLoopBuilder},
+};
+use crate::path;
+use std::process::exit;
 
 pub fn setup_and_run_tray() {
     let icon = load_icon();
     let event_loop = EventLoopBuilder::new().build().unwrap();
 
     let menu: Menu = Menu::new();
+    let open_terminal_item = MenuItem::new("Open Terminal", true, None::<Accelerator>);
     let restart_item = MenuItem::new("Restart", true, None::<Accelerator>);
     let quit_item = MenuItem::new("Quit", true, None::<Accelerator>);
 
-    let _ = menu.append_items(&[&restart_item, &quit_item]);
+    let _ = menu.append_items(&[&open_terminal_item, &restart_item, &quit_item]);
 
     let _tray_icon = Some(
         TrayIconBuilder::new()
@@ -26,16 +31,39 @@ pub fn setup_and_run_tray() {
     );
 
     let menu_channel = MenuEvent::receiver();
+    let event_loop_proxy = event_loop.create_proxy();
+
     let _ = event_loop.run(move |event, event_loop| {
         event_loop.set_control_flow(ControlFlow::Poll);
         if let Ok(event) = menu_channel.try_recv() {
-            if event.id == restart_item.id() {
-                restart_application();
-            } else if event.id == quit_item.id() {
-                std::process::exit(0);
+            match event.id {
+                id if id == restart_item.id() => restart_application(),
+                id if id == quit_item.id() => {
+                    std::process::exit(0);
+                },
+                id if id == open_terminal_item.id() => {
+                    // Signal the event loop to open a terminal-like window
+                    let _ = event_loop_proxy.send_event(());
+                }
+                _ => (),
             }
         }
+        if let Event::UserEvent(()) = event {
+            let _ = open_terminal_with_logs();
+        }
     });
+}
+
+fn open_terminal_with_logs() -> Result<(), Box<dyn std::error::Error>> {
+    let log_file_path = path::join_exe_dir_with_file("/logs/output.log")?;
+    let log_file_str = log_file_path.to_str().ok_or("Path contains invalid Unicode characters")?;
+    
+    Command::new("powershell")
+    .args(&["-NoExit", "-Command", &format!("Get-Content '{}' -Wait", log_file_str)])
+    .spawn() // Use spawn to launch the process without waiting for it to finish
+    .expect("Failed to execute command");
+
+    Ok(())
 }
 
 fn load_icon() -> tray_icon::Icon {
@@ -58,25 +86,8 @@ fn load_icon_from_bytes(bytes: &[u8]) -> tray_icon::Icon {
 }
 
 fn restart_application() {
-    use std::process::{exit, Command};
-
-    // Get the current executable path
-    let exe_path = match env::current_exe() {
-        Ok(path) => path,
-        Err(e) => {
-            error!("Failed to get the current executable path: {}", e);
-            return;
-        }
-    };
-
-    // Convert the path to a string, if possible
-    let exe_str = match exe_path.to_str() {
-        Some(s) => s,
-        None => {
-            error!("Failed to convert executable path to string.");
-            return;
-        }
-    };
+    let exe_path = path::current_exe_dir().unwrap();
+    let exe_str = path::path_to_str(&exe_path).unwrap();
 
     #[cfg(windows)]
     let _ = Command::new(exe_str)
